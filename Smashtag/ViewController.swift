1bc6f5423d8d75e71a5cfc4b10053e89a31bc502
Vito Valov <vitovalov@gmail.com>
2017-05-25 19:25:07 +0200

    init

---
+++ Smashtag/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Smashtag
+//
+//  Created by Vito on 25/05/2017.
+//  Copyright © 2017 Vitovalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+     
+}
+
