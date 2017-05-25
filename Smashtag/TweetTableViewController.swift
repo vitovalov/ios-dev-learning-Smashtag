f9d164250346ccdddc50502713878d2c79500a8e
Vito Valov <vitovalov@gmail.com>
2017-05-25 20:06:33 +0200

    connected to twitter

--- Smashtag/TweetTableViewController.swift
+++ Smashtag/TweetTableViewController.swift
@@ -7,9 +7,55 @@
 //
 
 import UIKit
+import Twitter
 
 class TweetTableViewController: UITableViewController {
 
+    private var tweets = [Array<Twitter.Tweet>]() {
+        didSet {
+            print(tweets)
+        }
+    }
+    
+    var searchText: String? {
+        didSet {
+            tweets.removeAll()
+            tableView.reloadData()
+            searchForTweets()
+            title = searchText
+        }
+    }
+    
+    // gonna return the request matching the search text
+    private func twitterRequest() -> Twitter.Request? {
+        if let query = searchText, !query.isEmpty {
+            return Twitter.Request(search: query, count: 100)
+        }
+        return nil
+    }
+    
+    private var lastTwitterRequest: Twitter.Request?
+    
+    private func searchForTweets() {
+        if let request = twitterRequest() {
+//            request.fetchTweets { newTweets in
+//                self.tweets.insert(newTweets, at: 0) // memory leak because user could exit this VC while the call hasn't completed yet (CLOSURE INSIDE VC)
+//            }
+            lastTwitterRequest = request
+            request.fetchTweets { [weak self] newTweets in
+                if request == self?.lastTwitterRequest {
+                    self?.tweets.insert(newTweets, at: 0)
+                    
+                }
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchText = "#stanford"
+    }
+    
     // MARK: - UITableDataSource methods Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
